82ff300787c3328517de84056249fb01c2940368
kademole70 <kademole70@gmail.com>
2025-07-11 18:26:50 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -6,4 +6,4 @@ export const terbit = '2025-04-10T00:03:13Z';
 export const google = 'QAEqPvZTgLe_Jf4dHTxAngrCbITf8MfSXp4wGYuC0h0';
 export const yandex = '4f5197d9f158bec9'
 
-export const index = '6efb56cb-17be-4271-bd20-6978bc416fde';
+export const index = '10c7d08f-4592-44eb-8f0f-cae98f5ae99d';
